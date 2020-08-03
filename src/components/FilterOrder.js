ac99ebdcf0fde0934ba635b5d40f9f1d60dfe3ba
Shayanne Samarasinghe <shayannesamarasinghe.com>
2020-08-03 16:31:55 -0400

    changes to package.json to make heroku run

--- src/components/FilterOrder.js
+++ src/components/FilterOrder.js
@@ -1,35 +1,32 @@
-import React from 'react'
+import React from "react";
 
-function FilterOrder (props){
-    function changeOrder (event){
-        const {name, value} = event.target
-        console.log(`[changeOrder], name(${name}) value(${value})`, event)
+function FilterOrder(props) {
+  function changeOrder(event) {
+    const { name, value } = event.target;
+    console.log(`[changeOrder], name(${name}) value(${value})`, event);
 
-        props.updateOrder (value)
-    }
+    props.updateOrder(value);
+  }
 
-
-    return(
-        <div class="mb-3 row">
-        <label for="inputPassword" class="col-sm-2 col-form-label">
-          Filter
-        </label>
-        <div class="col-sm-10">
-          <select onChange= {changeOrder} value= {props.order} class="form-select form-select-lg mb-3">
-            <option value="id">ID</option>  
-            <option value="name">Name</option>
-            <option value="OfficeLocation">Office Location</option>
-            <option value="Role">Role</option>
-          </select>
-        </div>
+  return (
+    <div class="mb-3 row">
+      <label for="inputPassword" class="col-sm-2 col-form-label">
+        Filter
+      </label>
+      <div class="col-sm-10">
+        <select
+          onChange={changeOrder}
+          value={props.order}
+          class="form-select form-select-lg mb-3"
+        >
+          <option value="id">ID</option>
+          <option value="name">Name</option>
+          <option value="OfficeLocation">Office Location</option>
+          <option value="Role">Role</option>
+        </select>
       </div>
-    )
-
-
-
+    </div>
+  );
 }
 
-
-
 export default FilterOrder;
-
