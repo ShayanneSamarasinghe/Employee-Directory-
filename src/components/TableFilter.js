197033ba50ad167d4b6d88a780b4aba25eeaf214
Shayanne Samarasinghe <shayannesamarasinghe.com>
2020-08-03 00:07:40 -0400

    added handleinput function to tablefilter

--- src/components/TableFilter.js
+++ src/components/TableFilter.js
@@ -1,13 +1,22 @@
 import React from 'react'
 
 function TableFilter (props){
+   function handleInputChange (event){
+    const { name, value} = event.target;
+    console.log(`[handleInputChange] name ${name} value ${value}`)
+
+
+    props.updateFilter (value)
+  }
+
+
     return(
         <div class="mb-3 row">
           <label for="name" class="col-sm-2 col-form-label">
             Name
           </label>
           <div class="col-sm-10">
-            <input type="text" class="form-control" id="name" value="George" />
+            <input onChange={handleInputChange} type="text" class="form-control" id="name" value={props.filter} />
           </div>
         </div>
 
